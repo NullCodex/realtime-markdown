28df0a7a2281f99a9e44eb9459ac4e671a250dbd
NullCodex <jamesonyu95@gmail.com>
2015-12-29 12:30:26 -0500

    Added newrelic support

---
+++ newrelic.js
@@ -0,0 +1,25 @@
+
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['realtime-markdown'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '9565f3f8cb65926d9c0c576a109ed0791bbf406e',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
